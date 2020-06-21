de6f0d1a450067e59e0eff63168564312630413a
Sachin <Sachin@DESKTOP-H57V1AC>
2020-06-21 10:33:31 -0700

    first commit

---
+++ src/main/java/com/banana/tech/onedropapp/OnedropappApplication.java
@@ -0,0 +1,13 @@
+package com.banana.tech.onedropapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedropappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnedropappApplication.class, args);
+	}
+
+}
